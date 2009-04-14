53c8d04564c2bd19574e7575c20d5e2f062ae3d4
Andreas Wolf <andreas.wolf@ikt-werk.de>
2009-04-14 17:48:50 +0000

    Added interface for the TCEforms context object
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/listmodule/branches/tceforms@495 743128fe-103e-dd11-99c4-001b210b3e58

--- t3lib/interfaces/interface.t3lib_tceforms_context.php
+++ t3lib/interfaces/interface.t3lib_tceforms_context.php
@@ -2,6 +2,11 @@
 
 interface t3lib_TCEforms_Context {
 
+	public function addHtmlForHiddenField($elementName, $code);
+
+	public function getEditFieldHelpMode();
+
+	public function setEditFieldHelpMode($mode);
 }
 
 ?>
