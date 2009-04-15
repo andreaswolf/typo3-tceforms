f3f3318cb8759c3879c6d07ef3a83470e6929023
Andreas Wolf <andreas.wolf@ikt-werk.de>
2009-04-15 15:38:17 +0000

    Added getIdentifier() to Record (fixes #3102)
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/listmodule/branches/tceforms@510 743128fe-103e-dd11-99c4-001b210b3e58

--- t3lib/tceforms/class.t3lib_tceforms_record.php
+++ t3lib/tceforms/class.t3lib_tceforms_record.php
@@ -237,6 +237,15 @@ class t3lib_TCEforms_Record {
 	}
 
 	/**
+	 * Returns a unique identifier of the form <table>:<uid> for the record
+	 *
+	 * @return string
+	 */
+	public function getIdentifier() {
+		return $this->getTable() . ':' . $this->recordData['uid'];
+	}
+
+	/**
 	 * Returns the context object this form is in.
 	 *
 	 * @return t3lib_TCEforms_ContextInterface
