f79e499ac0005059ce095a5267628ab62de63c7d
Andreas Wolf <andreas.wolf@ikt-werk.de>
2010-07-20 14:23:32 +0200

    Merged changes from r7085
    Fixed bug #1104: Setting file fields in TCA to "required" makes it
    impossible to save the record

--- t3lib/tceforms/element/class.t3lib_tceforms_element_group.php
+++ t3lib/tceforms/element/class.t3lib_tceforms_element_group.php
@@ -97,7 +97,7 @@ class t3lib_TCEforms_Element_Group extends t3lib_TCEforms_Element_Abstract {
 				if(!$disabled && !(isset($config['disable_controls']) && t3lib_div::inList($config['disable_controls'], 'upload'))) {
 						// Adding the upload field:
 					if ($this->TCEformsObject->edit_docModuleUpload && $config['uploadfolder']) {
-						$item .= '<input type="file" name="'.$this->itemFormElName_file.'"'.$this->TCEformsObject->formWidth().' size="60" />';
+						$item .= '<input type="file" name="'.$this->itemFormElName_file.'"'.$this->TCEformsObject->formWidth().' size="60" onchange="' . implode('', $this->fieldChangeFunc) . '" />';
 					}
 				}
 			break;
