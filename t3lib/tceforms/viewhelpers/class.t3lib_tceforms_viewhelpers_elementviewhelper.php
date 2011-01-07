bc27d188e3775c8d129f5efd6fb42151949d3887
Andreas Wolf <andreas.wolf@ikt-werk.de>
2011-01-07 12:03:09 +0100

    Several improvements to Fluid templates and the element ViewHelper
    * render record icon below form correctly (do not escape HTML)
    * elements in palettes are rendered correctly (both HTML semantics and
      look)

--- t3lib/tceforms/viewhelpers/class.t3lib_tceforms_viewhelpers_elementviewhelper.php
+++ t3lib/tceforms/viewhelpers/class.t3lib_tceforms_viewhelpers_elementviewhelper.php
@@ -7,9 +7,11 @@ class t3lib_TCEforms_ViewHelpers_ElementViewHelper extends Tx_Fluid_Core_ViewHel
 
 	/**
 	 * @param  t3lib_TCEforms_Element_Abstract $element The element
+	 * @param  t3lib_TCEforms_Record $record
+	 * @param  boolean $inPalette
 	 * @return void
 	 */
-	public function render(t3lib_TCEforms_Element_Abstract $element, t3lib_TCEforms_Record $record) {
+	public function render(t3lib_TCEforms_Element_Abstract $element, t3lib_TCEforms_Record $record, $inpalette = FALSE) {
 		//t3lib_div::debug($this->renderingContext);
 		if ($element instanceof t3lib_TCEforms_Element_Palette) {
 			return $this->viewHelperVariableContainer->getView()->renderPartial('elements', 'palette',
@@ -18,9 +20,18 @@ class t3lib_TCEforms_ViewHelpers_ElementViewHelper extends Tx_Fluid_Core_ViewHel
 					'record' => $record
 				)
 			);
+		} else {
+			if ($inpalette) {
+				return $element->getContents();
+			} else {
+				return $this->viewHelperVariableContainer->getView()->renderPartial('elements', 'default',
+					array(
+						'element' => $element,
+						'record' => $record
+					)
+				);
+			}
 		}
-		$ret = $element->renderContents();
-		return $ret;
 	}
 }
 
