0b57677c54a1b4c92f3ea8fda2d1f7ec48a92179
Benjamin Mack <benni@typo3.org>
2011-01-11 14:36:09 +0100

    Added linebreak functionality (not integrated right now)

--- t3lib/tceforms/element/class.t3lib_tceforms_element_linebreak.php
+++ t3lib/tceforms/element/class.t3lib_tceforms_element_linebreak.php
@@ -1,14 +1,9 @@
 <?php
 
-require_once(PATH_t3lib.'tceforms/element/class.t3lib_tceforms_element_abstract.php');
+class t3lib_TCEforms_Element_Linebreak extends t3lib_TCEforms_Element_Abstract {
 
-
-class t3lib_TCEforms_Element_Unknown extends t3lib_TCEforms_Element_Abstract {
 	protected function renderField() {
-
-		$item = 'Unknown type: '.$this->fieldSetup['config']['form_type'].'<br />';
-
-		return $item;
+		return '--Linebreak--';
 	}
 }
 ?>
