aaa8fc9279dbec6e9720d5a845726f2eb8fcba5b
Oliver Hader <oliver.hader@typo3.org>
2011-01-12 23:18:54 +0100

    Fixed naming of t3lib_TCEforms_IrreForm

--- t3lib/tceforms/class.t3lib_tceforms_irreform.php
+++ t3lib/tceforms/class.t3lib_tceforms_irreform.php
@@ -3,7 +3,7 @@
 /*
  * TODO check if inlineFirstPid may be replaced by a call to context object to get the pid
  */
-class t3lib_TCEforms_IRREForm extends t3lib_TCEforms_Form implements t3lib_TCEforms_NestableForm {
+class t3lib_TCEforms_IrreForm extends t3lib_TCEforms_Form implements t3lib_TCEforms_NestableForm {
 
 	/**
 	 * The element object containing this form.
@@ -200,7 +200,7 @@ class t3lib_TCEforms_IRREForm extends t3lib_TCEforms_Form implements t3lib_TCEfo
 	 * The configuration of this field
 	 *
 	 * @param $fieldConfig
-	 * @return t3lib_TCEforms_IRREForm $this
+	 * @return t3lib_TCEforms_IrreForm $this
 	 */
 	public function setFieldConfig(&$fieldConfig) {
 		$this->fieldConfig =& $fieldConfig;
