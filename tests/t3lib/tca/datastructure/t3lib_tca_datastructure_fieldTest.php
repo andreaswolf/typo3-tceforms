bcb3868be53f37f2f422f817babbe835385637f6
Andreas Wolf <andreas.wolf@ikt-werk.de>
2011-03-21 19:01:43 +0100

    [TASK] TCA: Fixed unit tests for Field class
    Also added one new test and @covers annotations where feasible

--- tests/t3lib/tca/datastructure/t3lib_tca_datastructure_fieldTest.php
+++ tests/t3lib/tca/datastructure/t3lib_tca_datastructure_fieldTest.php
@@ -8,18 +8,45 @@ class t3lib_tca_datastructure_fieldTest extends tx_phpunit_testcase {
 
 	protected static $localizationModeFixtures = array(
 		'exclude' => array(
-			array(array('l10n_mode' => 'exclude'))
+			array('l10n_mode' => 'exclude')
 		),
 		'mergeIfNotBlank' => array(
-			array(array('l10n_mode' => 'mergeIfNotBlank'))
+			array('l10n_mode' => 'mergeIfNotBlank')
 		),
 		'noCopy' => array(
-			array(array('l10n_mode' => 'noCopy'))
+			array('l10n_mode' => 'noCopy')
 		),
 		'prefixLangTitle' => array(
-			array(array('l10n_mode' => 'prefixLangTitle'))
+			array('l10n_mode' => 'prefixLangTitle')
 		)
 	);
+
+	/**
+	 * Data provider methods
+	 */
+
+	public static function validLocalizationModes() {
+		return self::$localizationModeFixtures;
+	}
+
+	/**
+	 * This provider adds an additional field configuration with a random, invalid localization mode, plus it adds a second,
+	 * boolean parameter that is TRUE if the l10n mode is valid and otherwise FALSE.
+	 *
+	 * @static
+	 * @return array
+	 */
+	public static function validLocalizationModesPlusOneInvalid() {
+		return array_merge(
+			self::$localizationModeFixtures,
+			array(
+				'invalidOne' => array(
+					array('l10n_mode' => uniqid()),
+					FALSE
+				)
+			)
+		);
+	}
 
 	/**
 	 * @test
@@ -42,7 +69,8 @@ class t3lib_tca_datastructure_fieldTest extends tx_phpunit_testcase {
 		);
 
 		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure');
-		$field = new t3lib_TCA_DataStructure_Field($mockedDataStructure, uniqid(), $configuration);
+		$mockedDataStructure->expects($this->once())->method('getFieldConfiguration')->will($this->returnValue($configuration));
+		$field = new t3lib_TCA_DataStructure_Field($mockedDataStructure, uniqid());
 
 		foreach ($configuration as $name => $value) {
 			$this->assertTrue($field->hasConfigurationValue($name));
@@ -52,30 +80,70 @@ class t3lib_tca_datastructure_fieldTest extends tx_phpunit_testcase {
 
 	/**
 	 * @test
+	 * @covers t3lib_TCA_DataStructure_Field::hasLocalizationMode
 	 */
-	public function localizationModeIsNotReturnedIfLanguageFieldIsNotSetInDataStructure() {
+	public function hasLocalizationModeReturnsFalseIfLanguageFieldInDataStructureIsNotSet() {
 		$fieldConfiguration = self::$localizationModeFixtures['exclude'];
-		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure', array('hasControlValue'));
-		$mockedDataStructure->expects($this->any())->method('hasControlValue')->with('languageField')->will($this->returnValue(FALSE));
-		$field = new t3lib_TCA_DataStructure_Field($mockedDataStructure, uniqid(), $fieldConfiguration);
+		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure', array('hasControlValue', 'getFieldConfiguration'));
+		$mockedDataStructure->expects($this->once())->method('hasControlValue')->with('languageField')->will($this->returnValue(FALSE));
+		$mockedDataStructure->expects($this->once())->method('getFieldConfiguration')->will($this->returnValue($fieldConfiguration));
 
-		$this->assertEquals('', $field->getLocalizationMode());
+		/** @var $mockedField t3lib_TCA_DataStructure_Field */
+		$mockedField = $this->getMock('t3lib_TCA_DataStructure_Field', array('hasConfigurationValue'), array($mockedDataStructure, uniqid()));
+		$mockedField->expects($this->never())->method('hasConfigurationValue');
+
+		$this->assertFalse($mockedField->hasLocalizationMode());
 	}
 
 	/**
 	 * @test
-	 * @dataProvider validLocalizationModes
+	 * @covers t3lib_TCA_DataStructure_Field::hasLocalizationMode
 	 */
-	public function getLocalizationModeReturnsAllValidValues($fieldConfiguration) {
-		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure', array('hasControlValue'));
-		$mockedDataStructure->expects($this->any())->method('hasControlValue')->with('languageField')->will($this->returnValue(TRUE));
-		$field = new t3lib_TCA_DataStructure_Field($mockedDataStructure, uniqid(), $fieldConfiguration);
+	public function hasLocalizationModeReturnsFalseIfConfigurationValueIsNotSet() {
+		$fieldConfiguration = self::$localizationModeFixtures['exclude'][0];
+		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure', array('hasControlValue', 'getFieldConfiguration'));
+		$mockedDataStructure->expects($this->once())->method('hasControlValue')->with('languageField')->will($this->returnValue(TRUE));
+		$mockedDataStructure->expects($this->once())->method('getFieldConfiguration')->will($this->returnValue($fieldConfiguration));
 
-		$this->assertEquals($fieldConfiguration['l10n_mode'], $field->getLocalizationMode());
+		/** @var $mockedField t3lib_TCA_DataStructure_Field */
+		$mockedField = $this->getMock('t3lib_TCA_DataStructure_Field', array('getConfigurationValue', 'hasConfigurationValue'), array($mockedDataStructure, uniqid()));
+		$mockedField->expects($this->atLeastOnce())->method('hasConfigurationValue')->with('l10n_mode')->will($this->returnValue(FALSE));
+		$mockedField->expects($this->any())->method('getConfigurationValue')->will($this->returnValue($fieldConfiguration['l10n_mode']));
+
+		$this->assertFalse($mockedField->hasLocalizationMode());
 	}
 
-	public static function validLocalizationModes() {
-		return self::$localizationModeFixtures;
+	/**
+	 * @test
+	 * @dataProvider validLocalizationModesPlusOneInvalid
+	 * @covers t3lib_TCA_DataStructure_Field::hasLocalizationMode
+	 */
+	public function hasLocalizationModeReturnsTrueOnlyForValidLocalizationModes($fieldConfiguration, $localizationModeValidity = TRUE) {
+		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure');
+		$mockedDataStructure->expects($this->once())->method('getFieldConfiguration')->will($this->returnValue($fieldConfiguration));
+		$mockedDataStructure->expects($this->once())->method('hasControlValue')->will($this->returnValue(TRUE));
+
+		/** @var $mockedField t3lib_TCA_DataStructure_Field */
+		$mockedField = $this->getMock('t3lib_TCA_DataStructure_Field', array('hasConfigurationValue'), array($mockedDataStructure, uniqid()));
+		$mockedField->expects($this->any())->method('hasConfigurationValue')->will($this->returnValue(TRUE));
+
+		$this->assertEquals($localizationModeValidity, $mockedField->hasLocalizationMode());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getLocalizationModeReturnsCorrectValueIfLocalizationModeIsPresent() {
+		$fieldConfiguration = array(
+			'l10n_mode' => uniqid()
+		);
+		$mockedDataStructure = $this->getMock('t3lib_TCA_DataStructure');
+		$mockedDataStructure->expects($this->once())->method('getFieldConfiguration')->will($this->returnValue($fieldConfiguration));
+		/** @var $mockedField t3lib_TCA_DataStructure_Field */
+		$mockedField = $this->getMock('t3lib_TCA_DataStructure_Field', array('hasLocalizationMode'), array($mockedDataStructure, uniqid()));
+		$mockedField->expects($this->any())->method('hasLocalizationMode')->will($this->returnValue(TRUE));
+
+		$this->assertEquals($fieldConfiguration['l10n_mode'], $mockedField->getLocalizationMode());
 	}
 
 	/**
